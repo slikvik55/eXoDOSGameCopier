d0c2b3973d4cf328e99fe7301b6e1c2bdb88e5b9
slikvik55 <slikvik55@yahoo.com>
2019-10-21 14:16:05 +0100

    Add project files.

---
+++ eXoDOSGameCopier/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace eXoDOSGameCopier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtRoot = new System.Windows.Forms.TextBox();
+            this.btnSelectRoot = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtXMLtoWrite = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSelectDest = new System.Windows.Forms.Button();
+            this.chkTestMode = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtRoot
+            // 
+            this.txtRoot.Location = new System.Drawing.Point(24, 48);
+            this.txtRoot.Name = "txtRoot";
+            this.txtRoot.Size = new System.Drawing.Size(319, 20);
+            this.txtRoot.TabIndex = 0;
+            // 
+            // btnSelectRoot
+            // 
+            this.btnSelectRoot.Location = new System.Drawing.Point(358, 32);
+            this.btnSelectRoot.Name = "btnSelectRoot";
+            this.btnSelectRoot.Size = new System.Drawing.Size(109, 36);
+            this.btnSelectRoot.TabIndex = 1;
+            this.btnSelectRoot.Text = "Select Root eXoDOS";
+            this.btnSelectRoot.UseVisualStyleBackColor = true;
+            this.btnSelectRoot.Click += new System.EventHandler(this.btnSelectRoot_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Root of Extracted eXoDOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folder with selected zipped games in.";
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(24, 96);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(319, 20);
+            this.txtDest.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(240, 148);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(103, 40);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Run!!";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRunTest_Click);
+            // 
+            // txtXMLtoWrite
+            // 
+            this.txtXMLtoWrite.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtXMLtoWrite.Location = new System.Drawing.Point(30, 216);
+            this.txtXMLtoWrite.Multiline = true;
+            this.txtXMLtoWrite.Name = "txtXMLtoWrite";
+            this.txtXMLtoWrite.Size = new System.Drawing.Size(522, 414);
+            this.txtXMLtoWrite.TabIndex = 6;
+            this.txtXMLtoWrite.WordWrap = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "New XML file";
+            // 
+            // btnSelectDest
+            // 
+            this.btnSelectDest.Location = new System.Drawing.Point(358, 80);
+            this.btnSelectDest.Name = "btnSelectDest";
+            this.btnSelectDest.Size = new System.Drawing.Size(109, 36);
+            this.btnSelectDest.TabIndex = 12;
+            this.btnSelectDest.Text = "Select Destination Folder";
+            this.btnSelectDest.UseVisualStyleBackColor = true;
+            this.btnSelectDest.Click += new System.EventHandler(this.btnSelectDest_Click);
+            // 
+            // chkTestMode
+            // 
+            this.chkTestMode.AutoSize = true;
+            this.chkTestMode.Checked = true;
+            this.chkTestMode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkTestMode.Location = new System.Drawing.Point(157, 161);
+            this.chkTestMode.Name = "chkTestMode";
+            this.chkTestMode.Size = new System.Drawing.Size(77, 17);
+            this.chkTestMode.TabIndex = 13;
+            this.chkTestMode.Text = "Test Mode";
+            this.chkTestMode.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 642);
+            this.Controls.Add(this.chkTestMode);
+            this.Controls.Add(this.btnSelectDest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtXMLtoWrite);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelectRoot);
+            this.Controls.Add(this.txtRoot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtRoot;
+        private System.Windows.Forms.Button btnSelectRoot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtXMLtoWrite;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSelectDest;
+        private System.Windows.Forms.CheckBox chkTestMode;
+    }
+}
+
---
+++ eXoDOSGameCopier/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace eXoDOSGameCopier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void ReadXML()
+        {
+            bool test = chkTestMode.Checked;
+            var files = Directory.GetFiles(txtDest.Text);
+            var subDirectory = "Data\\Platforms\\MS-DOS.xml";
+            var xmlPath = Path.Combine(txtRoot.Text, subDirectory);
+
+            string destPlatformFolder = "";
+            string destGamesFolder = "";
+            string destImagesFolder = "";
+            string destManualsFolder = "";
+
+            // Setup new folders 
+            if (!test)
+            {
+                destPlatformFolder = Directory.CreateDirectory(Path.Combine(txtDest.Text, "Data\\Platforms")).FullName;
+                destGamesFolder = Directory.CreateDirectory(Path.Combine(txtDest.Text, "eXoDOS\\Games")).FullName;
+                //string destDosFolder = Directory.CreateDirectory(Path.Combine(txtDest.Text, "eXoDOS\\Games\\!dos")).FullName;
+                destImagesFolder = Directory.CreateDirectory(Path.Combine(txtDest.Text, "Images")).FullName;
+                destManualsFolder = Directory.CreateDirectory(Path.Combine(txtDest.Text, "Manuals")).FullName;
+            }
+
+
+            // Read XML
+            XElement msDOSGames = XElement.Load(xmlPath);
+            IEnumerable<XElement> games = from item in msDOSGames.Descendants("Game") select item;
+            IEnumerable<XElement> additionalApps = from item in msDOSGames.Descendants("AdditionalApplication") select item;
+
+            // Temp XML Lists
+            List<XElement> gamesList = new List<XElement>();
+            List<IEnumerable<XElement>> appsList = new List<IEnumerable<XElement>>();
+
+            // Loop through each file.
+            for (int i = 0; i < files.Length; i++)
+            {
+                string zipName = Path.GetFileNameWithoutExtension(files[i]);
+
+                // Move zip to Games folder
+                if (!test)
+                    File.Move(files[i], Path.Combine(destGamesFolder, zipName + ".zip"));
+
+                IEnumerable<XElement> gameByAppPath = from item in games where ((string)item.Element("ApplicationPath")).Contains(zipName) select item;
+                if (gameByAppPath.Count() > 1)
+                {
+                    MessageBox.Show("Duplicate Game or ApplicationPath element for: " + zipName);
+                    return;
+                }
+                else
+                {
+                    gamesList.Add(gameByAppPath.First());
+
+                    var q = from item in gameByAppPath select new { ID = item.Element("ID").Value, RootFolder = item.Element("RootFolder").Value, ManualPath = item.Element("ManualPath").Value };
+                    string ID = q.FirstOrDefault().ID;
+                    string RootFolder = q.FirstOrDefault().RootFolder;
+                    string manualPath = q.FirstOrDefault().ManualPath;
+
+                    if (!test)
+                        DirectoryCopy(Path.Combine(txtRoot.Text, RootFolder), Path.Combine(txtDest.Text, RootFolder), true);
+
+                    if (!test)
+                        if (!String.IsNullOrWhiteSpace(manualPath))
+                            File.Copy(Path.Combine(txtRoot.Text, manualPath), Path.Combine(destManualsFolder, Path.GetFileName(manualPath)),true);
+
+                    IEnumerable<XElement> additionalAppByID = from item in additionalApps where ((string)item.Element("GameID")) == ID select item;
+
+                    appsList.Add(additionalAppByID);
+                }
+            }
+
+            XDocument msDOSnew = new XDocument(
+                new XDeclaration("1.0", null,"yes"),
+                new XElement("LaunchBox", gamesList.AsEnumerable<XElement>(), appsList.AsEnumerable<IEnumerable<XElement>>()));            
+
+            txtXMLtoWrite.Text = msDOSnew.Declaration + Environment.NewLine + msDOSnew.ToString();
+            if (!test)
+                File.WriteAllText(Path.Combine(destPlatformFolder, "MS-DOS.xml"), txtXMLtoWrite.Text);
+
+            MessageBox.Show("Finished!");
+        }
+
+        private void btnRunTest_Click(object sender, EventArgs e)
+        {
+            txtXMLtoWrite.Text = "";            
+            ReadXML();
+        }
+
+        private void btnSelectRoot_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtRoot.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnSelectDest_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtDest.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+    }
+}
